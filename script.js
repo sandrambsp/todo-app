7abb9ce05d3d380fc556b638c33fe2339d68ed58
sandrambsp <sandrambspereira@gmail.com>
2022-07-30 17:45:37 +0100

    corrected behavior when added task in filter

--- script.js
+++ script.js
@@ -20,25 +20,32 @@ function showTodo(filter) {
   let li = "";
   if (todos) {
     todos.forEach((todo, id) => {
-      let isCompleted = todo.status == "completed" ? "checked" : "";
-      if (filter == todo.status || filter == "all") {
+      let completed = todo.status == "completed" ? "checked" : "";
+      if (filter === todo.status || filter === "all") {
         li += `<li class="task">
-              <label for="${id}">
-                <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${isCompleted}/>
-                <p class="${isCompleted}">${todo.name}</p>
-              </label>
-              <div class="settings">
-                <i onclick="showMenu(this)" class="fa-solid fa-ellipsis"></i>
-                <ul class="task-menu">
-                  <li onclick="editTask(${id}, '${todo.name}')"><i class="fa-solid fa-pen"></i>Edit</li>
-                  <li onclick="deleteTask(${id})"><i class="fa-solid fa-trash"></i>Delete</li>
-                </ul>
-              </div>
-            </li>`;
+                <label for="${id}">
+                  <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${completed}/>
+                  <p class="${completed}">${todo.name}</p>
+                </label>
+                <div class="settings">
+                  <i onclick="showMenu(this)" class="fa-solid fa-ellipsis"></i>
+                  <ul class="task-menu">
+                    <li onclick="editTask(${id}, '${todo.name}')"><i class="fa-solid fa-pen"></i>Edit</li>
+                    <li onclick="deleteTask(${id}, "${filter}")"><i class="fa-solid fa-trash"></i>Delete</li>
+                  </ul>
+                </div>
+              </li>`;
       }
     });
   }
   taskBox.innerHTML = li || `<span>You don't have any tasks here<span>`;
+  let checkTask = taskBox.querySelectorAll(".task");
+  !checkTask.length
+    ? clearAll.classList.remove("active")
+    : clearAll.classList.add("active");
+  taskBox.offsetHeight >= 300
+    ? taskBox.classList.add("overflow")
+    : taskBox.classList.remove("overflow");
 }
 showTodo("all");
 
@@ -46,18 +53,22 @@ function editTask(taskId, taskName) {
   editId = taskId;
   isEditedTask = true;
   taskInput.value = taskName;
+  taskInput.focus();
+  taskInput.classList.add("active");
 }
 
-function deleteTask(deletedId) {
+function deleteTask(deletedId, filter) {
+  isEditedTask = false;
   todos.splice(deletedId, 1);
   localStorage.setItem("todo-list", JSON.stringify(todos));
-  showTodo("all");
+  showTodo(filter);
 }
 
 clearAll.addEventListener("click", () => {
+  isEditedTask = false;
   todos.splice(0, todos.length);
   localStorage.setItem("todo-list", JSON.stringify(todos));
-  showTodo("all");
+  showTodo();
 });
 
 function showMenu(selectedTask) {
@@ -86,9 +97,7 @@ taskInput.addEventListener("keyup", (event) => {
   let userTask = taskInput.value.trim();
   if (event.key == "Enter" && userTask) {
     if (!isEditedTask) {
-      if (!todos) {
-        todos = [];
-      }
+      todos = !todos ? [] : todos;
       let taskInfo = { name: userTask, status: "pending" };
       todos.push(taskInfo);
     } else {
@@ -97,6 +106,6 @@ taskInput.addEventListener("keyup", (event) => {
     }
     taskInput.value = "";
     localStorage.setItem("todo-list", JSON.stringify(todos));
-    showTodo("all");
+    showTodo(document.querySelector("span.active").id);
   }
 });
